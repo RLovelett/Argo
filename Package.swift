60a10c137d45769e71184ee13ccb32c52c6ebb39
Ryan Lovelett <ryan@lovelett.me>
2018-05-27 09:35:35 -0400

    Add Curry

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
   name: "Argo",
   dependencies: [
-    .Package(url: "https://github.com/thoughtbot/Runes.git", majorVersion: 4)
+    .Package(url: "https://github.com/thoughtbot/Runes.git", majorVersion: 4),
+    .Package(url: "https://github.com/thoughtbot/Curry.git", majorVersion: 4)
   ]
 )
